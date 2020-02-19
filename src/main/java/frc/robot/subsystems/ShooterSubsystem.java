ac0da720cf9a363b8a65df7fd92be7e6193a76d0
Dillon Shaffer <dillon.shaffer@K12.nd.us>
2020-02-19 16:50:54 -0600

    Updated Port Constants
    Moved the VictorSpx from pwm slot 3 to pwm slot 6

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
-    
+
     private final PWMVictorSPX m_ballShooterCommand = new PWMVictorSPX(Constants.ShooterConstants.kShooterControllerPort);
 
 
